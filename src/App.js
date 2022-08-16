2cfe94e24c219e0c97ac85ada2a7f74b175cf9ab
YDaKota <95923839+YDaKota@users.noreply.github.com>
2022-08-16 17:39:17 +0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Weather />
+        <Weather defaultCity="Kyiv" />
         <footer>
           This project was coded by Yulia and is {" "}
           <a href="https://github.com" target="_blank" rel="noreferrer noopener">
