7ab14ecfec7f9c61f418d1d4e170bd3945f23908
YDaKota <95923839+YDaKota@users.noreply.github.com>
2022-08-16 12:24:05 +0300

    Add the Git URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Yulia and is 
+        <a href="#" target="_blank">
+        open-sourced on Github
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
