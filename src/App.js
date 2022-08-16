25c1131d0ad68d0601115105ad81c49874697c8b
YDaKota <95923839+YDaKota@users.noreply.github.com>
2022-08-16 13:14:43 +0300

    main part of search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
-        <h1>Weather app</h1>
+        <Weather />
         <footer>
-          This project was coded by Yulia and is 
+          This project was coded by Yulia and is {" "}
           <a href="https://github.com" target="_blank" rel="noreferrer noopener">
           open-sourced on Github
           </a>
