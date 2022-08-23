2c3bb701131918a86dfdb0e36c55ecd0d2c85f19
YDaKota <95923839+YDaKota@users.noreply.github.com>
2022-08-23 14:27:17 +0300

    Add forecast functional

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className='container'>
         <Weather defaultCity="Kyiv" />
+        
         <footer>
           This project was coded by Yulia and is {" "}
           <a href="https://github.com" target="_blank" rel="noreferrer noopener">
